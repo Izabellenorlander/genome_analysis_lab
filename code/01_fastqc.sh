40c2fa6831766cbb77609b20078b7a5031dd7f24
Izabelle Norlander <izno1017@pelle1.uppmax.uu.se>
2026-05-15 16:35:26 +0200

    Update raw FastQC script paths

--- code/01_fastqc.sh
+++ code/01_fastqc.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
+#SBATCH -A uppmax2026-1-61
+#SBATCH -p pelle
+#SBATCH -c 2
+#SBATCH --mem=8G
+#SBATCH -t 01:00:00
+#SBATCH -J fastqc_raw
+#SBATCH -o /home/izno1017/genome_analysis_lab/slurm/fastqc_raw_%j.out
+
 module load FastQC
 
-fastqc ~/Genome_Analysis_Paper2/data/chr3_illumina_R1.fastq.gz \
-       ~/Genome_Analysis_Paper2/data/chr3_illumina_R2.fastq.gz \
-       -o ~/Genome_Analysis_Paper2/results/01_fastqc_raw
+RAW_DIR=/proj/uppmax2026-1-61/Genome_Analysis/2_Zhou_2023/reads/genomics_chr3_data
+OUT_DIR=/home/izno1017/genome_analysis_lab/results/01_fastqc_raw
+
+mkdir -p $OUT_DIR
+
+fastqc \
+  -t 2 \
+  -o $OUT_DIR \
+  $RAW_DIR/chr3_illumina_R1.fastq.gz \
+  $RAW_DIR/chr3_illumina_R2.fastq.gz
